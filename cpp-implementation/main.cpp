27269f3a1e3495b8b2981dfe9b0380ef141d0158
Ayush-vachhani <ayupatel107@gmail.com>
2024-11-24 11:55:57 +0530

    Modified C++ version

--- cpp-implementation/main.cpp
+++ cpp-implementation/main.cpp
@@ -2,37 +2,37 @@
 #include "parallelMergeSort.hpp"
 #include <chrono>
 #include <iostream>
+#include <vector>
 
 int main(int argc, char *argv[]) {
 
-  const int SIZE = 1000;
-  std::vector<int> nums(SIZE);
-  std::vector<int> nums1(SIZE);
+    const int SIZE = 10000000;
+    std::vector<int> nums(SIZE);
+    std::vector<int> nums1(SIZE);
 
-  for (int i = 0; i < SIZE; ++i) {
-    nums[i] = rand() % 1000;
-    nums1[i] = rand() % 1000;
-  }
+    // Fill nums and nums1 with random values
+    for (int i = 0; i < SIZE; ++i) {
+        nums[i] = rand() % 1000;
+        nums1[i] = rand() % 1000;
+    }
 
-  MergeSort *mergeSort = new MergeSort(&nums);
-  auto start = std::chrono::high_resolution_clock::now();
-  mergeSort->sort();
-  auto end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> mergeSortDuration = end - start;
-  std::cout << "MergeSort algorithm time taken: " << mergeSortDuration.count()
-            << "seconds" << std::endl;
+    // Pass by reference, not by pointer
+    MergeSort mergeSort(nums);
+    auto start = std::chrono::high_resolution_clock::now();
+    mergeSort.sort();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> mergeSortDuration = end - start;
+    std::cout << "MergeSort algorithm time taken: " << mergeSortDuration.count()
+              << " seconds" << std::endl;
 
-  delete mergeSort;
+    // Pass by reference, not by pointer
+    ParallelMergeSort mergeSort1(nums1);
+    start = std::chrono::high_resolution_clock::now();
+    mergeSort1.sort();
+    end = std::chrono::high_resolution_clock::now();
+    mergeSortDuration = end - start;
+    std::cout << "Parallel MergeSort algorithm time taken: "
+              << mergeSortDuration.count() << " seconds" << std::endl;
 
-  ParallelMergeSort *mergeSort1 = new ParallelMergeSort(&nums1);
-  start = std::chrono::high_resolution_clock::now();
-  mergeSort1->sort();
-  end = std::chrono::high_resolution_clock::now();
-  mergeSortDuration = end - start;
-  std::cout << "Parallel MergeSort algorithm time taken: "
-            << mergeSortDuration.count() << "seconds" << std::endl;
-
-  delete mergeSort1;
-
-  return 0;
+    return 0;
 }
